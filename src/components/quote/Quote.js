3e71791541c3b9557ccf891292cd0fa519b36930
Robert Mitroi <robert13mitroi@gmail.com>
2020-11-15 14:51:37 +0200

    feat: changed logic of styling to be decoupled from the component(:sad:) and added the tweet quote function

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -7,21 +7,31 @@ const API_URL =
 const PROXY = "https://cors-anywhere.herokuapp.com/";
 
 export default function Quote() {
-  const [bgColor, setBgColor] = useState("");
   const [quote, setQuote] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [disabled, setDisabled] = useState(true);
 
-  const twitter = {
-    twitterButton: {
-      backgroundColor: `${bgColor}`,
-    },
+  const tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.quoteText} - ${quote.quoteAuthor}`;
+    window.open(twitterUrl, "_blank");
   };
 
   const getQuote = async () => {
+    setDisabled(true);
     try {
       const res = await fetch(PROXY + API_URL);
       const data = await res.json();
       setQuote(data);
+      setLoading(false);
+      setDisabled(false);
     } catch (err) {
+      setQuote({
+        quote: {
+          quoteText: "Whoops, couldn't retrieve quote",
+          quoteAuthor: "Not Found",
+        },
+      });
+      setLoading(false);
       console.log("whoops, couldn't retrieve quote");
     }
   };
@@ -30,17 +40,23 @@ export default function Quote() {
     getQuote();
   }, []);
 
+  if (loading) return <div className="loader"></div>;
+
   return (
     <div style={styles.quoteContainer} id="quote-container">
       {/* Quote  */}
-      <div style={styles.quoteText}>
+      <div
+        style={
+          quote.quoteText.length < 120 ? styles.quoteText : styles.longQuote
+        }
+      >
         <i className="fa fa-quote-left" />
         <Text text={quote.quoteText} />
       </div>
 
       {/* Author */}
       <div style={styles.quoteAuthor}>
-        <Text text={quote.quoteAuthor} />
+        <Text text={quote.quoteAuthor ? quote.quoteAuthor : "Unknown"} />
       </div>
 
       {/* Buttons */}
@@ -48,12 +64,15 @@ export default function Quote() {
         <Button
           title="Tweet this quote!"
           icon="fab fa-twitter"
-          onMouseOver={() => setBgColor("#38a1f3")}
-          onMouseLeave={() => setBgColor("#333")}
-          style={twitter.twitterButton}
           id="twitter"
+          onClick={tweetQuote}
         />
-        <Button id="new-quote" text="New quote" onClick={getQuote} />
+        <Button
+          id="new-quote"
+          text="New quote"
+          disabled={disabled}
+          onClick={() => getQuote()}
+        />
       </div>
     </div>
   );
